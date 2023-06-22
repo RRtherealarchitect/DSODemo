01abcaf940358cefcbb0e1270019f6cfa70873e7
RRtherealarchitect <117695416+RRtherealarchitect@users.noreply.github.com>
2023-06-22 09:41:43 +0200

    added java file
    Sample

--- example.java
+++ example.java
@@ -1,4 +1,15 @@
-// Get username from parameters
+
+import java.io.File;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create a file object for the current location
+    File file = new File("newFile.txt");
+
+    try {
+
+      // Get username from parameters
 String username = request.getParameter("username");
 // Create a statement from database connection
 Statement statement = connection.createStatement();  
@@ -9,3 +20,16 @@ String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AN
 String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
 // Execute query and return the results
 ResultSet result = statement.executeQuery(query);
+      boolean value = file.createNewFile();
+      if (value) {
+        System.out.println("The new file is created.");
+      }
+      else {
+        System.out.println("The file already exists.");
+      }
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
